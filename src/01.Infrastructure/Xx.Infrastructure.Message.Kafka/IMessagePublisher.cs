044e5f56f6a181b74d752a4be218ab7d2126d7b7
aschenxiaoran <as_chenxiaoran@163.com>
2018-05-25 15:29:01 +0800

    更新消息发布接口

--- src/01.Infrastructure/Xx.Infrastructure.Message.Kafka/IMessagePublisher.cs
+++ src/01.Infrastructure/Xx.Infrastructure.Message.Kafka/IMessagePublisher.cs
@@ -6,6 +6,13 @@ namespace Xx.Infrastructure.Message.Kafka
 {
     public interface IMessagePublisher
     {
+        /// <summary>
+        /// 发布消息
+        /// </summary>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <param name="topicName"></param>
+        /// <param name="messageContent"></param>
+        /// <returns></returns>
         Task<MessageResult> PublishAsync<TMessage>(string topicName, TMessage messageContent) where TMessage : class, IKafkaMessage, new();
     }
 }
